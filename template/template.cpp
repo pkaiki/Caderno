765e3996822bfa00249d90af7efec8b3f1b1e391
Rafael <106854990+pkaiki@users.noreply.github.com>
2023-05-10 19:20:53 -0300

    Add files via upload

---
+++ template/template.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+void dbg_out() { cerr << endl; }
+template <typename H, typename... T> 
+void dbg_out(H h, T... t) { cout << ' ' << h; dbg_out(t...); }
+#define dbg(...) cout << #__VA_ARGS__ << ':'; dbg_out(__VA_ARGS__);
+#define ln cerr << "line:" << __LINE__ << " fine, thanks" << endl
+#define fi first
+#define se second
+#define int ll
+using ll = long long;
+#define endl '\n'
+
+#define INF 0x3f3f3f3f
+#define INFLL 0x3f3f3f3f3f3f3f3f
+#define oo 0x3f3f3f3f3f3f3f3f
+
+const int MOD = 1e9+7, N = 2e5+10;
+
+void solve(){
+}
+
+signed main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int t = 1; 
+    cin >> t;
+    while(t--) solve();
+
+    return 0;
+}
+
