a4c37b0f2aac4f0eb298f0dd7d33ff6670a68a18
Rafael <106854990+pkaiki@users.noreply.github.com>
2023-06-16 10:27:09 -0300

    Add files via upload

--- template/template.cpp
+++ template/template.cpp
@@ -1,33 +1,21 @@
-#include <bits/stdc++.h>
+include <bits/stdc++.h>
 using namespace std;
 void dbg_out() { cerr << endl; }
 template <typename H, typename... T> 
-void dbg_out(H h, T... t) { cout << ' ' << h; dbg_out(t...); }
-#define dbg(...) cout << #__VA_ARGS__ << ':'; dbg_out(__VA_ARGS__);
-#define ln cerr << "line:" << __LINE__ << " fine, thanks" << endl
-#define fi first
-#define se second
-#define int ll
-using ll = long long;
-#define endl '\n'
-
-#define INF 0x3f3f3f3f
-#define INFLL 0x3f3f3f3f3f3f3f3f
-#define oo 0x3f3f3f3f3f3f3f3f
-
-const int MOD = 1e9+7, N = 2e5+10;
+void dbg_out(H h, T... t) { cerr << ' ' << h; dbg_out(t...); }
+#define dbg(...) cerr << #__VA_ARGS__ << ':'; dbg_out(__VA_ARGS__);
+#define ln cerr << "line:" << __LINE__ << " bao, sow" << endl
+typedef long long ll;
+const int INF = 0x3f3f3f3f;
+const ll MOD = 1e9+7, INFLL = 0x3f3f3f3f3f3f3f3f, oo = INFLL;
 
 void solve(){
 }
 
-signed main(){
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    
-    int t = 1; 
-    cin >> t;
-    while(t--) solve();
-
-    return 0;
+int main(){
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  ll t = 1; 
+  while(t--) solve();
+  return 0;
 }
-
